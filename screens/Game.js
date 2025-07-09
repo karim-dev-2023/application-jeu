3862d0ab36733b83cb19ad2de8a3703f6fa035cf
Karim <karim.ayoub.job@gmail.com>
2025-07-09 12:16:23 +0200

    Add Game Script in the branch

--- screens/Game.js
+++ screens/Game.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Alert, StyleSheet, View, StatusBar,Button,Text } from "react-native";
 
 export default function Game() {
+  const [choice, setChoice] = useState("");
+  const baseNumber = Math.floor(Math.random() * 100);
+  const score = Math.floor(Math.random() * 100);
+
+  const navigation = useNavigation();
+  useEffect(() => {
+    if (choice) {
+      console.log(`base number =   ${baseNumber}  et score = ${score}`);
+      const winner =
+        (choice === "higher" && score > baseNumber) ||
+        (choice === "lower" && baseNumber > score);
+      Alert.alert(`You've ${winner ? "won" : "lost"}`, `You scored: ${score}`);
+      navigation.goBack();
+    }
+  }, [baseNumber, score, choice]);
+
   return (
-    <View>
-      <Text>Game Screen</Text>
+    <View styles={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text>Starting: {baseNumber}</Text>
+      <Button onPress={() => setChoice("higher")} title="Higher"/>
+      <Button onPress={() => setChoice("lower")} title="Lower"/>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
